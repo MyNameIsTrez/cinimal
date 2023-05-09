5ed2b50a998353e1a845ddbdb0c8b9b5b925a57b
MyNameIsTrez <welfje@gmail.com>
2023-05-10 01:11:04 +0200

    Allow deque to hold any type

--- tests/deque/deque.c
+++ tests/deque/deque.c
@@ -7,16 +7,22 @@ typedef struct Student {
 	double age;
 } Student;
 
+typedef struct Fruit {
+	char *name;
+} Fruit;
+
 typedef struct Deque {
-    Student * data;
+    uint8_t * elements;
+    int32_t type_size;
 	int32_t start_index;
 	int32_t size;
 	int32_t capacity;
 } Deque;
 
-Deque new(void) {
+Deque new(int32_t type_size) {
 	Deque deque = {};
-	deque.data = calloc(1, sizeof(Student));
+	deque.elements = calloc(1, type_size);
+	deque.type_size = type_size;
 	deque.capacity = 1;
 
 	return deque;
@@ -24,46 +30,70 @@ Deque new(void) {
 
 static void try_grow(Deque * const deque) {
 	if (deque->size == deque->capacity) {
-		Student * const new_data = calloc(deque->capacity * 2, sizeof(Student));
+		uint8_t * const new_elements = calloc(deque->capacity * 2, deque->type_size);
 
 		// An example ring buffer is [B,C, ,A], where the start_index is 3
 		int32_t trailing;
 		trailing = deque->capacity - deque->start_index;
 
-		// This copies the part that wrapped below index 0 to the other side
-		memcpy(new_data, deque->data + deque->start_index, trailing * sizeof(Student));
-		// This copies from index 0 on
-		memcpy(new_data + trailing, deque->data, (deque->size - trailing) * sizeof(Student));
+		// Copies everything to the right of the unused elements
+		memcpy(
+		    new_elements,
+		    deque->elements + deque->start_index * deque->type_size,
+		    trailing * deque->type_size
+		);
 
-		free(deque->data);
-		deque->data = new_data;
+		// Copies everything to the left of the unused elements
+		memcpy(
+		    new_elements + trailing * deque->type_size,
+		    deque->elements,
+		    (deque->size - trailing) * deque->type_size
+		);
+
+		free(deque->elements);
+		deque->elements = new_elements;
 
 		deque->start_index = 0;
 		deque->capacity *= 2;
 	}
 }
 
-static int32_t modulo(int32_t a, int32_t b) {
+static int32_t modulo(int32_t const a, int32_t const b) {
     return ((a % b) + b) % b;
 }
 
-void push_front(Deque * const deque, Student student) {
+static void assign_element(Deque const * const deque, uint8_t const * const element, int32_t const index) {
+	memcpy(deque->elements + index * deque->type_size, element, deque->type_size);
+}
+
+void push_front(Deque * const deque, uint8_t const * const element) {
 	try_grow(deque);
 
 	deque->start_index = modulo(deque->start_index - 1, deque->capacity);
-	deque->data[deque->start_index] = student;
+	assign_element(deque, element, deque->start_index);
 
 	deque->size++;
 }
 
-void push_back(Deque * const deque, Student student) {
+void push_back(Deque * const deque, uint8_t const * const element) {
 	try_grow(deque);
 
     int32_t index;
     index = (deque->start_index + deque->size) % deque->capacity;
-	deque->data[index] = student;
+	assign_element(deque, element, index);
 
 	deque->size++;
+}
+
+uint8_t * front(Deque * const deque) {
+	return deque->elements + deque->start_index * deque->type_size;
+}
+
+uint8_t * back(Deque * const deque) {
+    // TODO: Does this handle size=0 properly?
+    int32_t index;
+    index = (deque->start_index + deque->size - 1) % deque->capacity;
+	return deque->elements + index * deque->type_size;
 }
 
 void pop_front(Deque * const deque) {
@@ -71,50 +101,58 @@ void pop_front(Deque * const deque) {
 	deque->size--;
 }
 
-Student * front(Deque * const deque) {
-	return deque->data + deque->start_index;
-}
-
-Student * back(Deque * const deque) {
-    // TODO: Does this handle size=0 properly?
-    int32_t index;
-    index = (deque->start_index + deque->size - 1) % deque->capacity;
-	return deque->data + index;
-}
-
 void pop_back(Deque * const deque) {
 	deque->size--;
 }
 
 void delete(Deque * const deque) {
-	free(deque->data);
+	free(deque->elements);
+	deque->start_index = 0;
+	deque->size = 0;
 }
 
 int32_t main(void) {
 	Deque deque;
-	deque = new();
+	deque = new(sizeof(Student));
 
 	Student alice = {};
 	alice.age = 10;
-	push_back(&deque, alice);
+	push_back(&deque, (uint8_t *)&alice);
 
 	Student bob = {};
 	bob.age = 11;
-	push_back(&deque, bob);
+	push_back(&deque, (uint8_t *)&bob);
 
 	Student charles = {};
 	charles.age = 9;
-	push_front(&deque, charles);
+	push_front(&deque, (uint8_t *)&charles);
 
-	assert(deque.data[3].age == 9);
-	assert(deque.data[0].age == 10);
-	assert(deque.data[1].age == 11);
+	assert(((Student *)(deque.elements + 3 * deque.type_size))->age == 9);
+	assert(((Student *)(deque.elements + 0 * deque.type_size))->age == 10);
+	assert(((Student *)(deque.elements + 1 * deque.type_size))->age == 11);
 
-	assert(front(&deque)->age == 9);
+    Student * student;
+
+    student = (Student *)front(&deque);
+	assert(student->age == 9);
 	pop_front(&deque);
-	assert(back(&deque)->age == 11);
+
+    student = (Student *)back(&deque);
+	assert(student->age == 11);
 	pop_back(&deque);
 
+	delete(&deque);
+
+	deque = new(sizeof(Fruit));
+	Fruit apple;
+	apple.name = "apple";
+	push_back(&deque, (uint8_t *)&apple);
+	assert(
+	    strcmp(
+	        ((Fruit *)back(&deque))->name,
+	        "apple"
+	    ) == 0
+	);
 	delete(&deque);
 
 	return 0;
