1122fd88fc8b67bf6e8bf78c3328d99a45a93269
MyNameIsTrez <welfje@gmail.com>
2023-05-09 02:41:26 +0200

    Improve test `everything`

--- tests/everything/main.c
+++ tests/everything/main.c
@@ -4,46 +4,23 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-struct Names {
-	char * first_name;
-	char * last_name;
+enum Type {
+    INTEGER,
+    STRING,
 };
 
-typedef enum Country {
-	ukraine,
-	mexico,
-} Country;
-
-typedef struct Person {
-	double age;
-	struct Names names;
-	Country country;
-} Person;
-
-typedef enum {
-    INTEGER,
-    REAL,
-} Type;
-
 typedef struct {
-    Type type;
+    enum Type type;
     union {
         int integer;
-        float real;
+        char *string;
     };
 } Value;
 
-Value value_new_integer(int integer)
-{
-  Value v;
-  v.type = INTEGER;
-  v.integer = integer;
-  return v;
-}
-
-static void * allocate_one_person(void) {
-    return calloc(1, sizeof(Person));
+static void * allocate_one_value(void) {
+    return calloc(1, sizeof(Value));
 }
 
 static void add(int32_t * const n) {
@@ -52,8 +29,8 @@ static void add(int32_t * const n) {
 
 int32_t main(void) {
 	//single line comment
-	/*multi
-	line comment */ int32_t i = 0;
+	/*multiline
+	comment */ int32_t i = 0;
 
 	while (i < 3) {
 		if (i == 0) {
@@ -71,38 +48,29 @@ int32_t main(void) {
 	bark();
 	assert(get_paw_count() == 4);
 
-	assert(mexico == 1);
+	assert(STRING == 1);
 
-	Person * people = calloc(2, sizeof(Person));
+	Value * values = calloc(2, sizeof(Value));
 
-    Person * const frank = &people[0];
-	frank->age = 24 * 1.5;
-	assert(people[0].age == 36);
-	frank->names.last_name = "frank";
-	assert(strcmp(people[0].names.last_name, "frank") == 0);
+    Value * const v0 = &values[0];
+	v0->type = STRING;
+	assert(values[0].type == STRING);
+	assert(v0->string == NULL);
+	v0->string = "frank";
+	assert(strcmp(values[0].string, "frank") == 0);
 
-	assert(people[1].names.last_name == NULL);
+	values[1].type = STRING;
+	assert(values[1].type != INTEGER);
 
-	people[1].country = mexico;
-	assert(people[1].country != ukraine);
+	free(values);
+	values = allocate_one_value();
+	values[0].integer = 42;
+	assert(values[0].integer == 42);
+	free(values);
 
-	free(people);
-	people = allocate_one_person();
-	people[0].age = 42;
-	assert(people[0].age == 42);
-
-	free(people);
-	people = NULL;
-
-	Person friends[2][3] = {0};
-	friends[0][2].age = 13;
-	assert(friends[0][2].age == 13);
-
-    Value v;
-    v.integer = 42;
-    assert(v.integer == 42);
-    v.real = 1337;
-    assert(v.real == 1337);
+	Value matrix[2][3] = {0};
+	matrix[0][2].integer = 13;
+	assert(matrix[0][2].integer == 13);
 
 	printf("All tests passed!\n");
 
